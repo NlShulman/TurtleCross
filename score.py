c382fcc9275f573c8402860415d969eb6a87056c
Nicole Shulman <76790137+NlShulman@users.noreply.github.com>
2022-07-14 17:12:08 +0300

    Update score.py

--- score.py
+++ score.py
@@ -3,7 +3,7 @@ ALIGNMENT = "center"
 FONT = ('Courier', 20, 'bold')
 
 
-class Score(Turtle):
+class Level(Turtle):
 
     def __init__(self):
         super().__init__()
@@ -11,10 +11,11 @@ class Score(Turtle):
         self.hideturtle()
         self.color("white")
         self.penup()
-        self.goto(-220, 270)
+
         self.present_score()
 
     def present_score(self):
+        self.goto(-225, 265)
         self.write(f"LEVEL:{self.level}", align=ALIGNMENT, font=FONT)
 
     def update_score(self):
@@ -26,3 +27,8 @@ class Score(Turtle):
         self.clear()
         self.goto(0,  0)
         self.write(f"Game OVER \n    level: {self.level}", align=ALIGNMENT, font=FONT)
+
+    def reset_level(self):
+        self.clear()
+        self.level = 1
+        self.present_score()
