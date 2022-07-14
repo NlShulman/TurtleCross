2813ffcd3c4c91cf1f9194385d542bbcf2ed1735
Nicole Shulman <76790137+NlShulman@users.noreply.github.com>
2022-07-14 17:11:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,41 +3,52 @@ import time
 from turtle import Screen
 from turtle_crossing.cars import Cars
 from turtle_player import Player
-from score import Score
+from score import Level
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.tracer(0)
-screen.listen()
-screen.colormode(255)
 
-score = Score()
-player = Player()
-car = Cars()
-
-screen.onkeypress(key="Up", fun=player.move)
 # screen.onkey(key="Space", fun=restart_game)
 
 speed = 0.1
-game_on = True
+new_game = True
 
+while new_game:
+    game_on = True
+    screen = Screen()
+    screen.clearscreen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.tracer(0)
+    screen.listen()
+    screen.colormode(255)
 
-while game_on:
-    car.create_car()
-    car.move()
+    level = Level()
+    car = Cars()
+    player = Player()
 
-    for block in car.all_cars:
-        if block.distance(player) < 20:
-            game_on = False
-            score.game_over()
+    screen.onkeypress(key="Up", fun=player.move)
 
-    if player.ycor() > 280:
-        score.update_score()
-        player.goto(0, player.y)
-        speed -= 0.01
-    screen.update()
-    time.sleep(speed)
+    while game_on:
+
+        car.create_car()
+        car.move()
+
+        for block in car.all_cars:
+            if block.distance(player) < 20:
+                game_on = False
+                level.game_over()
+                user_choice = screen.textinput(title="PLAY AGAIN?", prompt=" Y\ N").lower()
+                if user_choice == "n":
+                    screen.bye()
+                elif user_choice == "y":
+                    player.reset_position()
+                    level.reset_level()
+
+        if player.ycor() > 280:
+            level.update_score()
+            player.goto(0, player.y)
+            speed -= 0.01
+        screen.update()
+        time.sleep(speed)
 
 
 screen.exitonclick()
